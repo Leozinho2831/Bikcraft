49a86a9aaccc756b5fa6af64d1b80a6ac85920de
Leozinho2831 <leoferraz1818@gmail.com>
2024-07-22 22:41:01 -0300

    commit: style large window to form in orcamento

---
+++ Assets/Javascript/formOrcamento.js
@@ -0,0 +1,22 @@
+const checkedInsurance = document.querySelector('#Seguro');
+const checkedBike = document.querySelector('#Bikcraft');
+const optionsBike = document.querySelector('.budget__bike');
+const optionsInsurance = document.querySelector('.budget__insurance');
+
+function choiceItem(){
+
+    if (checkedInsurance.checked && checkedBike.checked){
+        optionsBike.classList.remove('disableOption');
+        optionsInsurance.classList.remove('disableOption');
+    } else if(checkedBike.checked){
+        optionsBike.classList.remove('disableOption');
+        optionsInsurance.classList.add('disableOption');
+    } else if(checkedInsurance.checked){
+        optionsBike.classList.add('disableOption');
+        optionsInsurance.classList.remove('disableOption');
+    } else {
+        optionsBike.classList.add('disableOption');
+        optionsInsurance.classList.add('disableOption');
+    }
+
+};
